2d48da3659a58a0e0db9ac1b5ec1aaa77c095e78
Anuvabh Dutt <anuvabhdutt@gmail.com>
2012-03-27 15:44:55 +0530

    second commit

---
+++ application/controllers/events.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Events extends CI_Controller {
+
+	public function create(){}
+	public function view(){}
+	public function delete(){}
+	public function edit(){}
+	public function viewById($EID=-1){}
+	public function register($EID=-1){}
+	public function close(){}
+	public function viewAssociated($EID=-1){}
+	public function accredit(){}
+}
---
+++ application/views/index_view.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>SXC-CMS</title>
+    
+    <link rel="stylesheet" type="text/css" href="styles/960gs.css" />
+    <link rel="stylesheet" type="text/css" href="styles/home.css" />
+</head>
+
+<body>
+<div class="container_16" id="wrapper">
+
+  <div class="grid_16" id="header">
+  
+  	<div class="grid_4" id="emblem">
+    	<img src="images/150yrsLogo.jpg" style="height:190px;width: 200px;" />
+    </div>
+  	<div class="grid_11" id="banner"></div>
+  </div>
+    
+  <div class="grid_10" id="info">
+  	<p> lorem ipsum dorel set amet </p>
+  </div>
+  
+	<div class="grid_5" id="form">
+  	
+        <p>Log In To Explore</p>
+        <form method="post">
+            <table>
+                <tr>
+                    <td>Username:</td>
+                    <td><input type="text" name="username" /></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><input type="password" name="password" /></td>
+                </tr>
+            </table>
+            <input type="submit" value="Log In" name="submit" />
+        </form>
+        
+        <p><strong>New</strong> to the College? <br />
+        <a style="text-decoration:none;color:#900; "href="">Register Here</a></p>
+  	</div>
+	
+    <div class="grid_16" id="footer">
+    	&copy St. Xavier's College 2012
+    </div> 
+    
+</div>
+</body>
+</html>
